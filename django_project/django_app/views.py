7dbab3667d916e9ac52bb7184bff2c4dd6ce8887
ZachAtom <jacksonokram91@gmail.com>
2024-10-09 11:35:09 +0530

    login,logout,register django form

--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 from django.http import HttpResponse
 
-posts = [
-    {
-        'name': 'Jackson',
-        'roll': '12',
-        'address': 'Thongju',
-        'department': 'Computer Science'
-    },
-    {
-        'name': 'Tom',
-        'roll': '13',
-        'address': 'Singjamei',
-        'department': 'Physics'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'django_app/home.html',context)
 def about(request):
